cfca51215cb8ef69653dfa16da3f808fa994a82b
Aramis Vazquez <38926984+ANVazquez@users.noreply.github.com>
2019-01-28 02:03:51 -0500

    added a bow attack
    bow attack had some problems because it was not moving in the direction i wanted it to. I had to account for all possibilities meaning[if player was in the air, if player was on the ground, if player was moving, and if player was facing the other way].

--- src/Player.gd
+++ src/Player.gd
@@ -4,42 +4,79 @@ extends KinematicBody2D
 const SPEED = 80
 const GRAVITY = 13
 const JUMP_PWR = -250
-const FLOOR = Vector2(0, -1) 
+const FLOOR = Vector2(0, -1)
+#loads the scene
+const ARROW = preload("res://arrow.tscn")
 
 var velocity = Vector2()
 var grounded = false
+var bowAttack = false
+
+func _ready():
+	pass
 
 func _physics_process(delta):
 	
 	if Input.is_action_pressed("ui_right"):
-		velocity.x = SPEED
-		$AnimatedSprite.flip_h = false
-		$AnimatedSprite.play("run")
+		if bowAttack == false || is_on_floor() == false:
+			velocity.x = SPEED
+			if bowAttack == false:
+				$AnimatedSprite.play("run")
+				$AnimatedSprite.flip_h = false
+				#this is to make the arrow go in the right direction
+				if sign($Position2D.position.x) == -1:
+					$Position2D.position.x *= -1
 	elif Input.is_action_pressed("ui_left"):
-		velocity.x = -SPEED
-		$AnimatedSprite.flip_h = true
-		$AnimatedSprite.play("run")
+		if bowAttack == false || is_on_floor() == false:
+			velocity.x = -SPEED
+			if bowAttack == false:
+				$AnimatedSprite.play("run")
+				$AnimatedSprite.flip_h = true
+				#this is to make the arrow go in the left direction
+				if sign($Position2D.position.x) == 1:
+					$Position2D.position.x *= -1
 	else:
 		velocity.x = 0
-		$AnimatedSprite.play("idle")
+		if grounded == true && bowAttack == false:
+			$AnimatedSprite.play("idle")
 		
-	if Input.is_action_pressed("ui_select"):
-		if grounded == true:
-			velocity.y = JUMP_PWR
-			grounded = false
+	if Input.is_action_just_pressed("ui_up"):
+		if bowAttack == false:
+			if grounded == true:
+				velocity.y = JUMP_PWR
+				grounded = false
 		
-	velocity.y += GRAVITY
-	
-	if is_on_floor():
-		grounded = true
-	else:
-		grounded = false
-		if velocity.y < 0:
-			print("jumping")
-			$AnimatedSprite.play("jump")
+	if Input.is_action_just_pressed("shoot") && bowAttack == false:
+		if is_on_floor():
+			velocity.x = 0
+		bowAttack = true
+		$AnimatedSprite.play("bow_attack")
+		var arrow = ARROW.instance()
+		if sign($Position2D.position.x) == 1:
+			arrow.direction_of_arrow(1)
 		else:
-			$AnimatedSprite.play("fall")
+			arrow.direction_of_arrow(-1)
+		#this will get the stage 1 scene
+		get_parent().add_child(arrow)
+		arrow.position = $Position2D.global_position
+	
+	
+	velocity.y += GRAVITY
 	
 	velocity = move_and_slide(velocity, FLOOR)
 	
-	
+	if is_on_floor():
+		if grounded == false:
+			bowAttack = false
+		grounded = true
+	else:
+		if bowAttack == false:
+			grounded = false
+			if velocity.y < 0:
+				$AnimatedSprite.play("jump")
+			else:
+				$AnimatedSprite.play("fall")
+
+
+func _on_AnimatedSprite_animation_finished():
+	bowAttack = false
