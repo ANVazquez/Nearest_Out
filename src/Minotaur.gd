98c604267743710338cd0df7abe9a5892123f28f
Aramis Vazquez <38926984+ANVazquez@users.noreply.github.com>
2019-02-21 02:02:05 -0500

    Add files via upload

--- src/Minotaur.gd
+++ src/Minotaur.gd
@@ -2,30 +2,34 @@ extends KinematicBody2D
 
 #setting constants
 const GRAVITY = 10
-const SPEED = 30
 const FLOOR = Vector2(0, -1)
-
+#exports give you a chance to edit each spirte you have on screen in the inspector
+export (int) var speed = 30
+export (int) var hp = 10
 #variables
 var velocity = Vector2()
 #1 represents facing right
 var direction = 1
+var died = false
+var gotHit = false
 
 func _ready():
 	pass
 	
 func _physics_process(delta):
-	velocity.x = SPEED * direction
-	
-	if direction == 1:
-		$AnimatedSprite.flip_h = false
-	else:
-		$AnimatedSprite.flip_h = true
-	
-	$AnimatedSprite.play("walk")
-	
-	velocity.y += GRAVITY
-	
-	velocity = move_and_slide(velocity, FLOOR)
+	if died == false && gotHit == false:
+		velocity.x = speed * direction
+		
+		if direction == 1:
+			$AnimatedSprite.flip_h = false
+		else:
+			$AnimatedSprite.flip_h = true
+		
+		$AnimatedSprite.play("walk")
+		
+		velocity.y += GRAVITY
+		
+		velocity = move_and_slide(velocity, FLOOR)
 	
 	#is on wall detects if my sprite collided with a wall
 	if is_on_wall():
@@ -35,5 +39,30 @@ func _physics_process(delta):
 	if $RayCast2D.is_colliding() == false:
 		direction = direction * -1
 		$RayCast2D.position.x *= -1
-	
 		
+	if get_slide_count() > 0:
+		for x in range(get_slide_count()):
+			if "Player" in get_slide_collision(x).collider.name:
+				get_slide_collision(x).collider.died()
+	
+func dead():
+	gotHit = true
+	if gotHit == true:
+		hp -= 10
+		$AnimatedSprite.play("hit")
+		velocity = Vector2(0,0)
+		$Timer2.start()
+	if hp <= 0:
+		died = true
+		velocity = Vector2(0,0)
+		$AnimatedSprite.play("dying")
+		$CollisionShape2D.disabled = true
+		$Timer.start()
+
+#timers are great if you need something to be delayed but be careful 
+func _on_Timer_timeout():
+	queue_free()
+
+
+func _on_Timer2_timeout():
+	gotHit = false
